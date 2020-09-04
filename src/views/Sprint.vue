1dd5dd4f969216cde51386b992bc22072058b89d
Stephen O'Flynn <stephen.oflynn@gyron.net>
2020-09-04 18:15:32 +0200

    Wire basic ajax call and display

---
+++ src/views/Sprint.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="sprint">
+    <h1>Current Sprint</h1>
+    <div v-if="tasks.length === 0">{{ message }}</div>
+    <ul>
+      <li v-for="task in tasks" :key="task.iid">{{ task.title }}</li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+export default {
+  setup() {
+    const message = ref("No tasks in current sprint");
+    let tasks = ref([]);
+    onMounted(() => {
+      const token = process.env.VUE_APP_GITLAB_TOKEN;
+      const ajax = axios.create({
+        headers: {
+          'X-Requested-With': 'XmlHttpRequest',
+          'Accepts': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      ajax.get("https://gitlab.gyron.net/api/v4/projects/1/issues", {
+        params: {
+          state: "opened",
+          per_page: 100
+        }
+      })
+          .then(response => {
+            let tmpTasks;
+            tmpTasks = response.data.filter(task => {
+              return task.labels.includes("In Development") || task.labels.includes("Sprint") || task.labels.includes("Complete / Review");
+            });
+            tmpTasks.sort((a, b) => {
+              if (a.iid > b.iid) {
+                return 1;
+              }
+              if (a.iid < b.iid) {
+                return -1;
+              }
+              return 0;
+            });
+            tmpTasks.forEach(i => {
+              tasks.value.push(i);
+            });
+          })
+          .catch((error) => {
+            console.error(error)
+          });
+
+
+    });
+    return {message, tasks}
+  },
+  name: "Sprint"
+}
+</script>
+
+<style scoped>
+ul {
+  text-align: left;
+  margin: 0 5%;
+}
+
+</style>
